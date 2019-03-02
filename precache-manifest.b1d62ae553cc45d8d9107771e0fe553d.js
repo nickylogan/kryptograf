67f9e631c83e0b82f779d3ba8c965cea4ce4365b
NickyLogan <laurentiusdominick@yahoo.com>
2019-03-03 00:55:15 +0700

    Updates

---
+++ precache-manifest.b1d62ae553cc45d8d9107771e0fe553d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "35eb4110976e6605f9ee",
+    "url": "/kryptograf/static/js/runtime~main.35eb4110.js"
+  },
+  {
+    "revision": "5870f19a89b6262bf156",
+    "url": "/kryptograf/static/js/main.5870f19a.chunk.js"
+  },
+  {
+    "revision": "8694cfb011b9561cccf2",
+    "url": "/kryptograf/static/js/2.8694cfb0.chunk.js"
+  },
+  {
+    "revision": "5870f19a89b6262bf156",
+    "url": "/kryptograf/static/css/main.3fb917ec.chunk.css"
+  },
+  {
+    "revision": "47ea8949aac552391b03d054cdafe066",
+    "url": "/kryptograf/index.html"
+  }
+];
